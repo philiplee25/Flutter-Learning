dd16d065cfa9edd7f274343e44c858ba55437bc9
leepl <yplunap25@gmail.com>
2023-06-05 11:04:20 +0900

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'First app', // app을 총칭하는 이름, app 자체의 이름
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First app'), // appbar에서 보이는 제목
-      ),
-    );
-  }
+void main() {
+  runApp(const MyApp());
 }
