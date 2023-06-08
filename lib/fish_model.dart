3a44428c0bc44adf5a9465d0db759dc48a7f9e7e
leepl <yplunap25@gmail.com>
2023-06-08 19:42:05 +0900

    rest API floating button

--- lib/fish_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class FishModel with ChangeNotifier {
-  final String name;
-  final int number;
-  final String size;
-
-  FishModel({required this.name, required this.number, required this.size});
-}
